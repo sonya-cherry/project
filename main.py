bf4f2c4790de65a27f6d60c37ae1b6efeda0e5cd
Sofya <sofya.vishnevskaya0@yandex.ru>
2020-12-08 15:50:17 +0300

    Финал

--- main.py
+++ main.py
@@ -1,44 +1,86 @@
+import sys
+from PyQt5.QtGui import QMovie
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5 import uic
+
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.animation as animation
 
-l = 1.11 * (10 ** (-4))
-d_x = 0.01
-d_t = d_x ** 2 / (2 * l)
-T = 900
-J = round(T / d_t)
-K = round(1 / d_x)
-matrix = [[0] * K for _ in range(J)]
-t_min = 300
-t_max = 800
 
-for j in range(len(matrix[0]) // 2):
-    matrix[0][j] = t_min
-for j in range(len(matrix[0]) // 2, len(matrix[0])):
-    matrix[0][j] = t_max
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('gui.ui', self)
+        self._init_ui()
 
-for i in range(len(matrix)):
-    matrix[i][0] = t_min
-    matrix[i][len(matrix[0]) - 1] = t_max
+    def _init_ui(self):
+        self.setGeometry(100, 100, 1000, 800)
+        self.setWindowTitle('Теплопроводность')
+        self.graph.setText('')
 
-for i in range(1, len(matrix) - 1):
-    for j in range(1, len(matrix[0]) - 1):
-        matrix[i][j] = (d_t * l / (d_x ** 2)) * (
-                matrix[i - 1][j - 1] - 2 * matrix[i - 1][j] + matrix[i - 1][j + 1]) + \
-                       matrix[i - 1][j]
+        self.draw.clicked.connect(self._plot_graph)
 
-fig, ax = plt.subplots()
-line, = ax.plot(np.array(range(len(matrix[0]))), np.array(matrix[1]))
-# ax.set_xlim(0, len(matrix[0]))
-# ax.set_ylim(0, len(matrix[0]))
+    def _plot_graph(self):
+        try:
+            self.graph.setText('')
+            l = float(self.temp_val.text()) * 10 ** -int(self.temp_deg.text())
+            T = int(self.time_input.text())
+            t_min = int(self.min_temp_input.text())
+            t_max = int(self.max_temp_input.text())
+            make_plot(l, T, t_min, t_max)
+            self._load_gif()
+        except ValueError:
+            self.graph.setText('Неправильный формат ввода! Попробуйте другие значения')
+            return
 
-
-def update(x):
-    line.set_data(np.array(range(len(matrix[0]))), np.array(matrix[x]))
-    return line,
+    def _load_gif(self):
+        gif = QMovie('график.gif')
+        self.graph.setMovie(gif)
+        self.graph.setScaledContents(True)
+        gif.start()
 
 
-ani = animation.FuncAnimation(
-    fig, update, interval=5, blit=True, frames=range(len(matrix[0])), repeat=True)
+def make_plot(l, T, t_min, t_max):
+    d_x = 0.01
+    d_t = d_x ** 2 / (2 * l)
+    J = round(T / d_t)
+    K = round(1 / d_x)
+    matrix = [[0] * K for _ in range(J)]
 
-ani.save('график.gif', writer='ffmpeg', fps=30)
+    for j in range(len(matrix[0]) // 2):
+        matrix[0][j] = t_min
+    for j in range(len(matrix[0]) // 2, len(matrix[0])):
+        matrix[0][j] = t_max
+
+    for i in range(len(matrix)):
+        matrix[i][0] = t_min
+        matrix[i][len(matrix[0]) - 1] = t_max
+
+    for i in range(1, len(matrix) - 1):
+        for j in range(1, len(matrix[0]) - 1):
+            matrix[i][j] = (d_t * l / (d_x ** 2)) * (
+                    matrix[i - 1][j - 1] - 2 * matrix[i - 1][j] + matrix[i - 1][j + 1]) + \
+                           matrix[i - 1][j]
+
+    fig, ax = plt.subplots()
+    line, = ax.plot(np.array(range(len(matrix[0]))), np.array(matrix[1]))
+
+    def update(x):
+        line.set_data(np.array(range(len(matrix[0]))), np.array(matrix[x]))
+        return line,
+
+    ani = animation.FuncAnimation(
+        fig, update, interval=5, blit=True, frames=range(len(matrix[0])), repeat=True)
+
+    try:
+        ani.save('график.gif', writer='ffmpeg', fps=30)
+    except PermissionError:
+        pass
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Window()
+    ex.show()
+    sys.exit(app.exec())
--- project_gui.py
+++ project_gui.py
@@ -1,7 +1,11 @@
 import sys
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QMovie
 from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5 import uic
+
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
 
 
 class Window(QWidget):
@@ -18,19 +22,45 @@ class Window(QWidget):
         self.draw.clicked.connect(self._plot_graph)
 
     def _plot_graph(self):
-        try:
-            self.graph.setText('')
-            l = float(self.temp_val.text()) * 10 ** -int(self.temp_deg.text())
-            T = int(self.time_input.text())
-            t_min = int(self.min_temp_input.text())
-            t_max = int(self.max_temp_input.text())
-            self.return_values(l, T, t_min, t_max)
-        except ValueError:
-            self.graph.setText('Неправильный формат ввода! Попробуйте другие значения')
-            return
+        gif = QMovie('график.gif')
+        self.graph.setMovie(gif)
+        self.graph.setScaledContents(True)
+        gif.start()
 
-    def return_values(self, l, T, t_min, t_max):
-        return l, T, t_min, t_max
+
+def make_plot(l, T, t_min, t_max):
+    d_x = 0.01
+    d_t = d_x ** 2 / (2 * l)
+    J = round(T / d_t)
+    K = round(1 / d_x)
+    matrix = [[0] * K for _ in range(J)]
+
+    for j in range(len(matrix[0]) // 2):
+        matrix[0][j] = t_min
+    for j in range(len(matrix[0]) // 2, len(matrix[0])):
+        matrix[0][j] = t_max
+
+    for i in range(len(matrix)):
+        matrix[i][0] = t_min
+        matrix[i][len(matrix[0]) - 1] = t_max
+
+    for i in range(1, len(matrix) - 1):
+        for j in range(1, len(matrix[0]) - 1):
+            matrix[i][j] = (d_t * l / (d_x ** 2)) * (
+                    matrix[i - 1][j - 1] - 2 * matrix[i - 1][j] + matrix[i - 1][j + 1]) + \
+                           matrix[i - 1][j]
+
+    fig, ax = plt.subplots()
+    line, = ax.plot(np.array(range(len(matrix[0]))), np.array(matrix[1]))
+
+    def update(x):
+        line.set_data(np.array(range(len(matrix[0]))), np.array(matrix[x]))
+        return line,
+
+    ani = animation.FuncAnimation(
+        fig, update, interval=5, blit=True, frames=range(len(matrix[0])), repeat=True)
+
+    ani.save('график.gif', writer='ffmpeg', fps=30)
 
 
 if __name__ == '__main__':
